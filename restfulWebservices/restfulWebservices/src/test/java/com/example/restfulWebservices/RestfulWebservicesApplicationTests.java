ac9628e094ba92fa8271f1dee1726bce4c5e4eaa
teamtech-cpu <83945511+teamtech-cpu@users.noreply.github.com>
2024-02-16 11:58:10 +0530

    intial commit

---
+++ restfulWebservices/restfulWebservices/src/test/java/com/example/restfulWebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulWebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
